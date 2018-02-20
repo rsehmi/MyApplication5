a9a8f76c39561364ca4cad7a25e94fe5bfe48f5f
Rajbir Sehmi <rsehmi@google.com>
2018-02-20 15:02:04 -0800

    Demo Branch Something

--- app/src/main/java/com/example/rsehmi/myapplication/MyBranch.java
+++ app/src/main/java/com/example/rsehmi/myapplication/MyBranch.java
@@ -1,7 +1,11 @@
 package com.example.rsehmi.myapplication;
 
+import android.util.Log;
+
 public class MyBranch {
     void doSomething() {
         System.out.println("abc");
+        Log.d("Asd", "doSomething: hello Woel");
+
     }
 }
